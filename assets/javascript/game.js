8c0439724cef17dd2cba8905bfdbb48082ba91f0
David Levens <52017470+Davidlevens@users.noreply.github.com>
2019-08-24 21:36:31 -0400

    Styling images in css to no avail

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,115 +1,51 @@
-var computerChoices = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z"
-  ];
-  var wins = 0;
-  var losses = 0;
-  var chances = 15;
-  var guessArray = [];
-  var confirmPsychic = confirm("Are you a psychic?");
-  if (confirmPsychic) {
-    alert("Is there a chance you have powers of the supernatural?");
-    alert("See if you can guess which letter the computer is thinking of.");
-  } else {
-    alert("Wrong Answer, Try Again");
-    location.reload();
-  }
-  
-  // alert("Is there a chance you have powers of the supernatural?");
-  // alert("See if you can guess which letter the computer is thinking of.");
-  
-  // Setup event function for key press
-  document.onkeyup = function(event) {
-    var userGuess = event.key;
-  
-    var computerGuess =
-      computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  
-    console.log(userGuess); //This is to log the key press
-  
-    // Checks to insure that the userGuess is in the array of letters.
-    if (computerChoices.includes(userGuess)) {
-      // If the user guess matches computer guess, you must have ESP
-      if (userGuess === computerGuess) {
-        wins++;
-        guessArray.push(userGuess);
-        chances--;
-        console.log("Wins: " + wins);
-      }
-      // If the user guess doesn't match, don't quit your day job
-      else if (userGuess !== computerGuess) {
-        losses++;
-        guessArray.push(userGuess);
-        chances--;
-        console.log("Losses: " + losses);
-      }
-      // Chances = 0
-      if (chances === 0) {
-        alert("You had your chance, you aren't a psychic!!");
-        location.reload();
-      }
-      // Losses Reaches 10
-      else if (losses === 10) {
-        alert(
-          "You have guessed wrong 10 times. You are not a psychic, and probably just average."
-        );
-        location.reload();
-      }
-      // Wins = 10, oh and be afraid of this person
-      // There was a 1 in 26 chance they'd get it right...
-      else if (wins === 10) {
-        alert(
-          "Someone call the Ghostbusters, or an exorcist!! You're an enigma!!"
-        );
-        location.reload();
-      }
-    } else {
-      // If the user enters any character other than a letter
-      alert("That was not a letter!");
-    }
-    var html =
-      "<p>You chose: " +
-      userGuess +
-      "</p>" +
-      "<p>You previously guessed: " +
-      guessArray +
-      "</p>" +
-      "<p>The computer was thinking: " +
-      computerGuess +
-      "</p>" +
-      "<p>wins: " +
-      wins +
-      "</p>" +
-      "<p>losses: " +
-      losses +
-      "</p>" +
-      "<p>chances: " +
-      chances +
-      "</p>";
-    // Set the inner HTML contents of the #game div to our html string
-    document.querySelector("#game").innerHTML = html;
+// The specific letters that the user typed.
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Setting for zero
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+var letterUser = [];
+var eachofLetters = null;
+
+
+// Sets the computerGuess variable equal to a random choice from the computerChoice array.
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+function countGuessesLeft() {
+	document.querySelector("#guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+}
+
+function farUserGuesses() {
+	document.querySelector("#letter").innerHTML = "Your Guesses so far: " + letterUser.join(' ');
+}
+
+countGuessesLeft();
+
+var restart = function() {
+	guessesLeft = 9;
+	letterUser = [];
+	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+}
+
+// When the user presses a key, it will run the following function..
+document.onkeyup = function(event) {
+	guessesLeft--;
+
+	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+	letterUser.push(userGuess);
+	countGuessesLeft();
+	farUserGuesses();
+
+	if (userGuess === computerGuess){
+		wins++;
+		document.querySelector("#wins").innerHTML = "Wins: " + wins;
+		restart();
+	} 
+	else if (guessesLeft === 0) {
+		losses++;
+		document.querySelector("#lose").innerHTML = "Loses: " + losses;
+		restart();
+	}
   };
